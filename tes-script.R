95f94c3d6fb8307de18b2b5c9a673c866fe82b64
kai-s-suzuki <kssuzuki7@gmail.com>
2025-05-16 09:16:18 -0700

    changing color in visualization to be a different gradient

---
+++ tes-script.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(lterdatasampler)
+
+ggplot(data = pie_crab,
+       aes(x = latitude,
+           y = size,
+           color = latitude)) +
+  geom_point() +
+  scale_color_gradient(low = "#217832",
+                       high = "#124923")
